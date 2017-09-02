448667beee44ecdcdc0181744b47065f69275f5c
张新罡 <zhangxingang@yupiao.com>
2017-09-02 21:02:20 +0800

    update

--- file4goroutine.go
+++ file4goroutine.go
@@ -1,18 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
-	"time"
 	"sync"
+	"time"
 )
 
+// 有四个线程1、2、3、4。线程1的功能就是输出1，线程2的功能就是输出2，以此类推.........
+// 现在有四个文件ABCD。初始都为空。现要让四个文件呈如下格式：
+
+// A：1 2 3 4 1 2....
+// B：2 3 4 1 2 3....
+// C：3 4 1 2 3 4....
+//D：4 1 2 3 4 1....
+
 var num int
-type Printnum struct{
+
+type Printnum struct {
 	lock *sync.Mutex
-	sum int
+	sum  int
 }
 
-func (p *Printnum) Incr() int{
+func (p *Printnum) Incr() int {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 	p.sum++
@@ -21,49 +30,49 @@ func (p *Printnum) Incr() int{
 
 var printnum = Printnum{
 	lock: new(sync.Mutex),
-	sum:0,
+	sum:  0,
 }
 
-func PrintN(i int){
+func PrintN(i int) {
 	for {
-		go func(i int){
+		go func(i int) {
 			for {
-				if printnum.Incr() == 100{
+				if printnum.Incr() == 100 {
 					return
 				}
-				<- chlista[i-1]
-				fmt.Print("a",i)
-				cha <- i%4+1
+				<-chlista[i-1]
+				fmt.Print("a", i)
+				cha <- i%4 + 1
 			}
 		}(i)
-		go func(i int){
+		go func(i int) {
 			for {
-				if printnum.Incr() == 100{
+				if printnum.Incr() == 100 {
 					return
 				}
-				<- chlistb[i-1]
-				fmt.Print("b",i)
-				chb <- i%4+1
+				<-chlistb[i-1]
+				fmt.Print("b", i)
+				chb <- i%4 + 1
 			}
 		}(i)
-		go func(i int){
+		go func(i int) {
 			for {
-				if printnum.Incr() == 100{
+				if printnum.Incr() == 100 {
 					return
 				}
-				<- chlistc[i-1]
-				fmt.Print("c",i)
-				chc <- i%4+1
+				<-chlistc[i-1]
+				fmt.Print("c", i)
+				chc <- i%4 + 1
 			}
 		}(i)
-		go func(i int){
+		go func(i int) {
 			for {
-				if printnum.Incr() == 100{
+				if printnum.Incr() == 100 {
 					return
 				}
-				<- chlistd[i-1]
-				fmt.Print("d",i)
-				chd <- i%4+1
+				<-chlistd[i-1]
+				fmt.Print("d", i)
+				chd <- i%4 + 1
 			}
 		}(i)
 	}
@@ -73,56 +82,55 @@ var ch1a = make(chan int)
 var ch2a = make(chan int)
 var ch3a = make(chan int)
 var ch4a = make(chan int)
-var chlista = []chan int{ch1a,ch2a,ch3a,ch4a}
+var chlista = []chan int{ch1a, ch2a, ch3a, ch4a}
 
 var ch1b = make(chan int)
 var ch2b = make(chan int)
 var ch3b = make(chan int)
 var ch4b = make(chan int)
-var chlistb = []chan int{ch1b,ch2b,ch3b,ch4b}
+var chlistb = []chan int{ch1b, ch2b, ch3b, ch4b}
 
 var ch1c = make(chan int)
 var ch2c = make(chan int)
 var ch3c = make(chan int)
 var ch4c = make(chan int)
-var chlistc = []chan int{ch1c,ch2c,ch3c,ch4c}
+var chlistc = []chan int{ch1c, ch2c, ch3c, ch4c}
 
 var ch1d = make(chan int)
 var ch2d = make(chan int)
 var ch3d = make(chan int)
 var ch4d = make(chan int)
-var chlistd = []chan int{ch1d,ch2d,ch3d,ch4d}
+var chlistd = []chan int{ch1d, ch2d, ch3d, ch4d}
 
 var cha = make(chan int) // 1,2,3,4
 var chb = make(chan int) // 1,2,3,4
 var chc = make(chan int) // 1,2,3,4
 var chd = make(chan int) // 1,2,3,4
 
-
-func filea(){
-	for{
-		num := <- cha
-		chlista[num-1] <- num 
+func filea() {
+	for {
+		num := <-cha
+		chlista[num-1] <- num
 	}
 }
 
-func fileb(){
-	for{
-		num := <- chb
+func fileb() {
+	for {
+		num := <-chb
 		chlistb[num-1] <- num
 	}
 }
 
-func filec(){
-	for{
-		num := <- chc
+func filec() {
+	for {
+		num := <-chc
 		chlistc[num-1] <- num
 	}
 }
 
-func filed(){
-	for{
-		num := <- chd
+func filed() {
+	for {
+		num := <-chd
 		chlistd[num-1] <- num
 	}
 }
@@ -138,12 +146,10 @@ func main() {
 	go filec()
 	go filed()
 
-
 	chd <- 1
 	cha <- 4
 	chb <- 3
 	chc <- 2
 
-	time.Sleep(time.Second*10)
+	time.Sleep(time.Second * 10)
 }
-
