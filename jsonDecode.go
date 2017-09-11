d638d1b7dac47f0cbe131008cdfad9d68ccf3dd3
张新罡 <zhangxingang@yupiao.com>
2017-09-11 16:08:25 +0800

    update

--- jsonDecode.go
+++ jsonDecode.go
@@ -6,15 +6,16 @@ import (
 )
 
 type A struct {
-	AA int `json:"aa,omitempty"`
-	BB int `json:"bb,string,omitempty"`
+	AA *int `json:"aa,omitempty"`
+	BB *int `json:"bb,omitempty"`
 }
 
 func main() {
 	a := A{}
-	bytes := []byte("{\"bb\":\"2\"}")
+	bytes := []byte("{\"bb\":2}")
 	json.Unmarshal(bytes, &a)
-	fmt.Println(a)
+	fmt.Printf("%#v\n", a)
+	fmt.Println(*a.BB)
 	str, _ := json.Marshal(a)
 	fmt.Println(string(str))
 }
--- test/benchmark_test.go
+++ test/benchmark_test.go
@@ -2,14 +2,15 @@ package test
 
 import (
 	"errors"
+	"net/http"
 	"testing"
 )
 
-func BenchmarkAA(b *testing.B) {
-	for i := 0; i < b.N; i++ { //use b.N for looping
-		Division(4, 5)
-	}
-}
+// func BenchmarkAA(b *testing.B) {
+// 	for i := 0; i < b.N; i++ { //use b.N for looping
+// 		Division(4, 5)
+// 	}
+// }
 
 func Division(a, b float64) (float64, error) {
 	if b == 0 {
@@ -19,9 +20,18 @@ func Division(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
+func BenchmarkHttp(b *testing.B) {
+	URL := "http://b2bmsa-dev-develop.showbiz.wepiao.com/ads"
+	cli := http.Client{}
+	for i := 0; i < b.N; i++ {
+		cli.Get(URL)
+		//fmt.Println(resp)
+	}
+}
+
 /*
 	性能测试：
-	go test -test.bench="." -count=5 -cpuprofile=cpu.profile
+	go test -test.bench="." -count=5 -cpuprofile=cpu.profile -benchmem
 	通过cpu.prof 看性能数据
-	go tool pprof popcnt.test cpu.profile
+	go tool pprof popcnt.test cpu.profileq
 */
